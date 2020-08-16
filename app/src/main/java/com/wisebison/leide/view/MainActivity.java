456ff47adb20c3cf52cd4742178c027da1996c91
goldthorp <simon@goldthorp.com>
2020-08-16 16:40:47 -0400

    remove comment

--- app/src/main/java/com/wisebison/leide/view/MainActivity.java
+++ app/src/main/java/com/wisebison/leide/view/MainActivity.java
@@ -60,7 +60,6 @@ public class MainActivity extends AppCompatActivity {
     // as you specify a parent activity in AndroidManifest.xml.
     final int id = item.getItemId();
 
-    //noinspection SimplifiableIfStatement
     if (id == R.id.action_settings && backupUtil != null) {
       backupUtil.restore();
       return true;
